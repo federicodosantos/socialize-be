078a23eb3162f25dfa3bbbedb50012095b9c8bbc
Gede Indra Adi Brata <indrabrata599@gmail.com>
2024-12-03 04:42:22 +0700

    chore: update jenkinesfile

--- test/user_repo_test.go
+++ test/user_repo_test.go
@@ -32,13 +32,13 @@ func createUser() *model.User {
 	now := time.Now()
 
 	return &model.User{
-		Name: "Jamal",
+		Name:  "Jamal",
 		Email: "jamalunyu@gmail.com",
 		Photo: sql.NullString{
 			String: "https://nctojleerwtdfjlhafsq.supabase.co/storage/v1/object/public/photo_profile/pas%20foto%20windah.jpg",
-			Valid: true,
+			Valid:  true,
 		},
-		Password: "rahasia123",
+		Password:  "rahasia123",
 		CreatedAt: now,
 		UpdatedAt: now,
 	}
@@ -46,9 +46,9 @@ func createUser() *model.User {
 
 func TestCreateUser(t *testing.T) {
 	type testCase struct {
-		name string
-		setupMock func(mock sqlmock.Sqlmock, user *model.User)
-		user *model.User
+		name          string
+		setupMock     func(mock sqlmock.Sqlmock, user *model.User)
+		user          *model.User
 		expectedError error
 	}
 
@@ -59,16 +59,16 @@ func TestCreateUser(t *testing.T) {
 				mock.ExpectQuery(regexp.QuoteMeta(fmt.Sprintf(`
 								SELECT COUNT(*) FROM users WHERE email = '%s'
 								`, user.Email))).
-    			WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(0))
+					WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(0))
 
 				mock.ExpectExec(regexp.QuoteMeta(fmt.Sprintf(`
 								INSERT INTO users(name, email, password, created_at, updated_at)
   								VALUES ('%s', '%s', '%s', '%s', '%s')`,
-								user.Name, user.Email, user.Password, util.ConvertTimeToString(user.CreatedAt), util.ConvertTimeToString(user.UpdatedAt)))).
-				WillReturnResult(sqlmock.NewResult(1, 1))
+					user.Name, user.Email, user.Password, util.ConvertTimeToString(user.CreatedAt), util.ConvertTimeToString(user.UpdatedAt)))).
+					WillReturnResult(sqlmock.NewResult(1, 1))
 
 			},
-			user: createUser(),
+			user:          createUser(),
 			expectedError: nil,
 		},
 		{
@@ -77,9 +77,9 @@ func TestCreateUser(t *testing.T) {
 				mock.ExpectQuery(regexp.QuoteMeta(fmt.Sprintf(`
 								SELECT COUNT(*) FROM users WHERE email = '%s'
 								`, user.Email))).
-    			WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
+					WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
 			},
-			user: createUser(),
+			user:          createUser(),
 			expectedError: customerror.ErrEmailExist,
 		},
 	}
@@ -114,71 +114,71 @@ func TestCreateUser(t *testing.T) {
 
 func TestGetUserByEmail(t *testing.T) {
 	type testCase struct {
-		name           string
+		name          string
 		email         string
-		setupMock      func(mock sqlmock.Sqlmock, email string)
-		expectedUser   *model.User
-		expectedError  error
+		setupMock     func(mock sqlmock.Sqlmock, email string)
+		expectedUser  *model.User
+		expectedError error
 	}
 
 	user := createUser()
 
 	testCases := []testCase{
 		{
-			name: "Success get user by email",
+			name:  "Success get user by email",
 			email: user.Email,
 			setupMock: func(mock sqlmock.Sqlmock, email string) {
 				mock.ExpectQuery(regexp.QuoteMeta(fmt.Sprintf("SELECT * FROM users WHERE email = '%s'", email))).
-				WillReturnRows(sqlmock.NewRows([]string{"id", "name", "email", "password", "created_at", "updated_at"}).
-				AddRow(user.ID, user.Name, user.Email, user.Password, user.CreatedAt, user.UpdatedAt))
+					WillReturnRows(sqlmock.NewRows([]string{"id", "name", "email", "password", "created_at", "updated_at"}).
+						AddRow(user.ID, user.Name, user.Email, user.Password, user.CreatedAt, user.UpdatedAt))
 			},
-			expectedUser: user,
+			expectedUser:  user,
 			expectedError: nil,
 		},
 		{
-			name: "Error user not found",
+			name:  "Error user not found",
 			email: user.Email,
 			setupMock: func(mock sqlmock.Sqlmock, email string) {
 				mock.ExpectQuery(regexp.QuoteMeta(fmt.Sprintf("SELECT * FROM users WHERE email = '%s'", email))).
-				WillReturnRows(sqlmock.NewRows([]string{"id", "name", "email", "password", "created_at", "updated_at"}))
+					WillReturnRows(sqlmock.NewRows([]string{"id", "name", "email", "password", "created_at", "updated_at"}))
 			},
-			expectedUser: nil,
+			expectedUser:  nil,
 			expectedError: customerror.ErrEmailNotFound,
 		},
 	}
 
 	for _, tc := range testCases {
-        t.Run(tc.name, func(t *testing.T) {
-            db, mock, err := setup()
-            if err != nil {
+		t.Run(tc.name, func(t *testing.T) {
+			db, mock, err := setup()
+			if err != nil {
 				t.Fatalf("error to create sql mock and db due to %s", err.Error())
-            }
-            defer db.Close()
+			}
+			defer db.Close()
 
-            tc.setupMock(mock, tc.email)
+			tc.setupMock(mock, tc.email)
 
-            u := repository.NewUserRepo(db)
+			u := repository.NewUserRepo(db)
 
-            user , err := u.GetUserByEmail(context.Background(), tc.email)
-            
-            if tc.expectedError != nil {
-                assert.Error(t, err)
-                assert.ErrorIs(t, err, tc.expectedError)
-                assert.Nil(t, user)
-            } else {
-                assert.NoError(t, err)
-                assert.NotNil(t, user)
-                assert.Equal(t, tc.expectedUser.ID, user.ID)
-                assert.Equal(t, tc.expectedUser.Name, user.Name)
-                assert.Equal(t, tc.expectedUser.Email, user.Email)
-                assert.Equal(t, tc.expectedUser.Password, user.Password)
-                assert.WithinDuration(t, time.Now().Local(), user.CreatedAt, time.Second)
-                assert.WithinDuration(t, time.Now().Local(), user.UpdatedAt, time.Second)
-            }
+			user, err := u.GetUserByEmail(context.Background(), tc.email)
 
-            if err := mock.ExpectationsWereMet(); err != nil {
+			if tc.expectedError != nil {
+				assert.Error(t, err)
+				assert.ErrorIs(t, err, tc.expectedError)
+				assert.Nil(t, user)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, user)
+				assert.Equal(t, tc.expectedUser.ID, user.ID)
+				assert.Equal(t, tc.expectedUser.Name, user.Name)
+				assert.Equal(t, tc.expectedUser.Email, user.Email)
+				assert.Equal(t, tc.expectedUser.Password, user.Password)
+				assert.WithinDuration(t, time.Now().Local(), user.CreatedAt, time.Second)
+				assert.WithinDuration(t, time.Now().Local(), user.UpdatedAt, time.Second)
+			}
+
+			if err := mock.ExpectationsWereMet(); err != nil {
 				t.Errorf("there were unfulfilled expectations: %s", err)
 			}
-        })
-    }
+		})
+	}
 }
