8e148d714b93c3eea471a0818f36b47cf15e277a
Gede Indra Adi Brata <indrabrata599@gmail.com>
2024-12-03 01:34:47 +0700

    feat: add regex for password

--- pkg/database/mysql/connection.go
+++ pkg/database/mysql/connection.go
@@ -3,18 +3,17 @@ package mysql
 import (
 	"fmt"
 	"log"
-	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 )
 
 func DBInit() *sqlx.DB {
-	username := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	database := os.Getenv("DB_NAME")
+	username := "root"
+	password := ""
+	host := "db"
+	port := "3306"
+	database := "db-socialize"
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		username, password, host, port, database)
